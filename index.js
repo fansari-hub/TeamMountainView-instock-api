76bc36f3d256710b90ce85a54cad512baed021f7
CrisL305 <cristopher@allapattahcdc.org>
2024-08-29 19:50:58 -0400

    made changes to line 7 by correctly putting the CORS syntax and added app.use(express.json())

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
+
 const cors = require("cors");
 require('dotenv').config();
 
-app.use("cors");
+app.use(cors());
+
 app.use(express.static('public'));
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
