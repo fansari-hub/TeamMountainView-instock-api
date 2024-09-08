17ba3e8cee0103800878e288273ba9354411b6df
Nicole Liu <109707278+nliub@users.noreply.github.com>
2024-09-08 14:18:10 -0700

    completed ticket

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -15,7 +15,9 @@ const warehouseList = async (req, res) => {
 // ************** GET WAREHOUSE BY ID ************
 const getSingleWarehouse = async (req, res) => {
   try {
-    const warehouseDataFound = await knex("warehouses").where({ id: req.params.id });
+    const warehouseDataFound = await knex("warehouses").where({
+      id: req.params.id,
+    });
 
     if (warehouseDataFound.length === 0) {
       return res.status(404).json({
@@ -32,13 +34,86 @@ const getSingleWarehouse = async (req, res) => {
   }
 };
 
+// ************** EDIT/PUT WAREHOUSE BY ID ************
+const updateWarehouse = async (req, res) => {
+  //check if warehouse exists
+  try {
+    const warehouseFound = await knex("warehouses").where({
+      id: req.params.id,
+    });
+
+    if (warehouseFound.length === 0) {
+      return res.status(404).json({
+        message: `Warehouse with ID ${req.params.id} not found.`,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: `Unable to access inventory data ${req.params.id}`,
+    });
+  }
+
+  //check for missing fields
+  if (
+    !req.body.warehouse_name ||
+    !req.body.address ||
+    !req.body.city ||
+    !req.body.country ||
+    !req.body.contact_name ||
+    !req.body.contact_position ||
+    !req.body.contact_phone ||
+    !req.body.contact_email
+  ) {
+    return res.status(400).json({
+      message: "Missing one or more required fields",
+      data_received: req.body,
+    });
+  }
+
+  //validate phone number has exactly 11 digits
+
+  if (req.body.contact_phone.replace(/\D/g, "").length !== 11) {
+    return res.status(400).json({
+      message:
+        "Invalid phone number. It must contain 11 digits including area code",
+    });
+  }
+
+  //validate email ends with "@instock.com"
+  if (!req.body.contact_email.endsWith("@instock.com")) {
+    return res.status(400).json({
+      message: "Invalid email. It must have the domain @instock.com",
+    });
+  }
+
+  //update database
+  try {
+    const _result = await knex("warehouses")
+      .where({ id: req.params.id })
+      .update(req.body);
+
+    const updatedWarehouse = await knex("warehouses").where({
+      id: req.params.id,
+    });
+    res.status(200).json(updatedWarehouse[0]);
+  } catch (error) {
+    res.status(500).json({
+      message: `Unable to update inventory item: ${error}`,
+    });
+  }
+};
+
 // ************** REMOVE WAREHOUSE ************
 const removeWarehouse = async (req, res) => {
   try {
-    const rowsDeleted = await knex("warehouses").where({ id: req.params.id }).delete();
+    const rowsDeleted = await knex("warehouses")
+      .where({ id: req.params.id })
+      .delete();
 
     if (rowsDeleted === 0) {
-      return res.status(404).json({ message: `Warehouse with ID ${req.params.id} not found or remove operation was not succesfull` });
+      return res.status(404).json({
+        message: `Warehouse with ID ${req.params.id} not found or remove operation was not succesfull`,
+      });
     }
 
     res.sendStatus(204);
@@ -49,10 +124,10 @@ const removeWarehouse = async (req, res) => {
   }
 };
 
-
 // ************** EXPORTS************
 module.exports = {
   warehouseList,
   getSingleWarehouse,
   removeWarehouse,
+  updateWarehouse,
 };
--- routes/warehouse-routes.js
+++ routes/warehouse-routes.js
@@ -6,37 +6,65 @@ const SERVER_LOGGING = true;
 router.use((req, _res, next) => {
   next();
   if (SERVER_LOGGING) {
-      console.log(`Requested METHOD: ${req.method} | ENDPOINT: ${req.originalUrl} | CODE: ${req.res.statusCode}`);
+    console.log(
+      `Requested METHOD: ${req.method} | ENDPOINT: ${req.originalUrl} | CODE: ${req.res.statusCode}`
+    );
   }
 });
 
+router
+  .route("/")
+  .get((req, res) => {
+    warehouseController.warehouseList(req, res);
+  })
+  .post((req, res) => {
+    // Placeholder: Remove line below when controller is ready to be used.
+    res.send("You have reached /warehouses POST");
+  });
 
-router.get("/", (req, res) => {
-  warehouseController.warehouseList(req, res); 
-});
-
-router.post("/", (req, res) => {
-   //Placeholder: Remove line below when controller is ready to be used.
-  res.send("You have reached /warehouses POST"); 
-});
-
-router.get("/:id", (req, res) => {
-  warehouseController.getSingleWarehouse(req, res);
-});
+router
+  .route("/:id")
+  .get((req, res) => {
+    warehouseController.getSingleWarehouse(req, res);
+  })
+  .delete((req, res) => {
+    warehouseController.removeWarehouse(req, res);
+  })
+  .put((req, res) => {
+    warehouseController.updateWarehouse(req, res);
+  });
 
 router.get("/:id/inventories", (req, res) => {
-   //Placeholder: Remove line below when controller is ready to be used.
+  // Placeholder: Remove line below when controller is ready to be used.
   res.send("You have reached /warehouses ID GET inventory LIST");
 });
 
-router.delete("/:id", (req, res) => {
-  warehouseController.removeWarehouse(req, res);
-});
+// router.get("/", (req, res) => {
+//   warehouseController.warehouseList(req, res);
+// });
 
-router.put("/:id", (req, res) => {
-   //Placeholder: Remove line below when controller is ready to be used.
-  res.send("You have reached /warehouses ID PUT");
-});
+// router.post("/", (req, res) => {
+//    //Placeholder: Remove line below when controller is ready to be used.
+//   res.send("You have reached /warehouses POST");
+// });
+
+// router.get("/:id", (req, res) => {
+//   warehouseController.getSingleWarehouse(req, res);
+// });
+
+// router.get("/:id/inventories", (req, res) => {
+//    //Placeholder: Remove line below when controller is ready to be used.
+//   res.send("You have reached /warehouses ID GET inventory LIST");
+// });
+
+// router.delete("/:id", (req, res) => {
+//   warehouseController.removeWarehouse(req, res);
+// });
+
+// router.put("/:id", (req, res) => {
+//    //Placeholder: Remove line below when controller is ready to be used.
+//   res.send("You have reached /warehouses ID PUT");
+// });
 
 // ************** EXPORTS************
 module.exports = router;
