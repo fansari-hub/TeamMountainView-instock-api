4ef5b4240fb7fc96f35efb5253f2e015102b0ea8
farid.ansari <farid.ansari@gmail.com>
2024-09-15 14:29:06 -0700

    removed commented-out code, applied formatting to all files using prettier.

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ app.use(express.json());
 
 const PORT = process.env.PORT;
 
-const warehouseRoutes = require('./routes/warehouse-routes.js')
-const inventoryRoutes = require('./routes/inventory-routes.js')
-app.use('/api/warehouses', warehouseRoutes );
-app.use('/api/inventory', inventoryRoutes);
+const warehouseRoutes = require("./routes/warehouse-routes.js");
+const inventoryRoutes = require("./routes/inventory-routes.js");
+app.use("/api/warehouses", warehouseRoutes);
+app.use("/api/inventory", inventoryRoutes);
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
